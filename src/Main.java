9a45f7b7cb283ac628b0171b643080b4b7c19f40
Chronos <igorgabrielantunes252002@gmail.com>
2025-10-15 20:24:48 -0300

    Finalizando Desafio

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.Dev;
 
 public class Main {
 
     public static void main(String[] args) {
-        Curso curso = new Curso();
-        curso.setTitulo("Curso Java");
-        curso.setDescricao("Descrição curso Java");
-        curso.setCargaHoraria(8);
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso JavaScript");
@@ -19,16 +21,38 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(java.time.LocalDate.now());
 
-        Mentoria mentoria2 = new Mentoria();
-        mentoria2.setTitulo("Mentoria de JavaScript");
-        mentoria2.setDescricao("Descrição mentoria JavaScript");
-        mentoria2.setData(java.time.LocalDate.now());
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso);
-        System.out.println(curso2);
-        System.out.println(mentoria);
-        System.out.println(mentoria2);
-        
+        Dev devIgor = new Dev();
+        devIgor.setNome("Igor");
+        devIgor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Igor" + devIgor.getConteudosInscritos());
+
+        devIgor.progredir();
+        devIgor.progredir();
+        System.out.println("--");
+        System.out.println("Conteudos Inscritos Igor" + devIgor.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Igor" + devIgor.getConteudosConcluidos());
+        System.out.println("XP:" + devIgor.calcularTotalXp());
+
+       System.out.println("===================="); 
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos João" + devJoao.getConteudosInscritos());
+
+        devJoao.progredir();
+        System.out.println("--");
+        System.out.println("Conteudos Inscritos João" + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos João" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
+
     }
     
 }
